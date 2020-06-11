6c2f0d0a3329025dddcc9e8e09b802e6700b6418
Eorium <travisgent312@gmail.com>
2020-06-11 17:22:29 -0600

    add finished assignment with styling inside of spaceList.js and other components

--- src/spaceList.js
+++ src/spaceList.js
@@ -1,9 +1,25 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import styled from 'styled-components';
 
 const SpaceList = () => {
-
     const [spaceImage, setSpaceImage] = useState([]);
+
+    const ImageStyle = styled.img`
+        width: 64%;
+        height: 36%;
+        background-position: center;
+        padding-top: 2%;
+    `;
+    const PImageTitle = styled.p`
+        font-size: 1.4rem;
+        padding-top: 1%;
+    `;
+    const DivImageExplanation = styled.div`
+        width: 60%;
+        font-size: 1rem;
+        margin: 1% auto;
+    `;
     useEffect(() => {
         axios
         .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
@@ -13,7 +29,13 @@ const SpaceList = () => {
         })
         .catch(error => console.log(error));
     }, []);
-    return (<img className="pictureDiv" src={spaceImage.url}></img>)
+    return (
+        <div>
+            <ImageStyle src={spaceImage.url}></ImageStyle>
+            <PImageTitle>{spaceImage.title}{". Image Taken: "}{spaceImage.date}</PImageTitle>
+            <DivImageExplanation>{spaceImage.explanation}</DivImageExplanation>
+        </div>
+    )
 };
 
 export default SpaceList;
